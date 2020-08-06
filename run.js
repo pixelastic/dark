e87f6f0ff9b5ad94e940b7f9d4640261ac8afbd4
Pixelastic <tim@pixelastic.com>
2020-08-06 23:19:35 +0200

    wip: All characters

--- run.js
+++ run.js
@@ -5,37 +5,36 @@ const exists = require('firost/lib/exists');
 const download = require('firost/lib/download');
 const run = require('firost/lib/run');
 const writeJson = require('firost/lib/writeJson');
-const readJson = require('firost/lib/readJson');
 const _ = require('golgoth/lib/lodash');
 
+const hx = async (filepath, selector, attribute) => {
+  const command = `hx "${filepath}" "${selector}" "${attribute}"`;
+  const { stdout } = await run(command, {
+    shell: true,
+    stdout: false,
+  });
+  return stdout;
+};
+
 (async () => {
-  const world = 'eva';
-  const urls = await read(`./tmp/${world}.txt`);
+  const worlds = ['adam', 'eva', 'original'];
   const characters = [];
-  await pMap(
-    urls.split('\n'),
-    async (url) => {
+  await pMap(worlds, async (world) => {
+    const urls = await read(`./tmp/${world}.txt`);
+    await pMap(urls.split('\n'), async (url) => {
       const fullUrl = `https://dark-netflix.fandom.com/${url}`;
       const localFile = './tmp/cache/' + urlToFilepath(fullUrl);
       if (!(await exists(localFile))) {
         await download(fullUrl, localFile);
       }
 
-      const { stdout: title } = await run(
-        `hx ${localFile} "meta[name=twitter:title]" content`,
-        {
-          shell: true,
-          stdout: false,
-        }
-      );
+      const title = await hx(localFile, 'meta[name=twitter:title]', 'content');
       const name = title.replace(' | Dark Wiki | Fandom', '');
 
       // Get images
-      const command = `hx ${localFile} ".pi-image-thumbnail"`;
-      const { stdout } = await run(command, { shell: true, stdout: false });
-      const imgs = JSON.parse(stdout);
+      const imgs = JSON.parse(await hx(localFile, '.pi-image-thumbnail', ''));
 
-      _.each(imgs, (img) => {
+      _.each(_.castArray(imgs), (img) => {
         const time = img.alt;
         const picture = img.src;
         characters.push({
@@ -43,13 +42,12 @@ const _ = require('golgoth/lib/lodash');
           world,
           time,
           picture,
+          wikiUrl: fullUrl,
         });
       });
-    },
-  );
+    });
+  });
 
   const dataPath = './lib/data.json';
-  const data = await readJson(dataPath);
-  const newData = _.concat(data, characters);
-  await writeJson(newData, dataPath);
+  await writeJson(characters, dataPath);
 })();
