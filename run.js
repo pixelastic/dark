3cd64e31ae5613a24180f3cf8ed7c6d4636a69e5
Pixelastic <tim@pixelastic.com>
2020-08-07 16:07:56 +0200

    wip: before runnning new script

--- run.js
+++ run.js
@@ -2,37 +2,39 @@ const pMap = require('golgoth/lib/pMap');
 // const read = require('firost/lib/read');
 // const urlToFilepath = require('firost/lib/urlToFilepath');
 // const exists = require('firost/lib/exists');
-// const download = require('firost/lib/download');
+const download = require('firost/lib/download');
 // const run = require('firost/lib/run');
 const writeJson = require('firost/lib/writeJson');
 const readJson = require('firost/lib/readJson');
 const _ = require('golgoth/lib/lodash');
 
 (async () => {
-  const characters = await readJson('./lib/data.json');
+  const characters = await readJson('./src/_data/characters.json');
   const newData = [];
   await pMap(characters, async (character) => {
-    const { wikiUrl, name, world, time, picture } = character;
-    const slug = _.camelCase(`${name}-${time}-${world}`);
+    const name = character.name;
+    const [firstName, _lastName] = _.words(name);
+    const lastName = _.upperCase(_lastName);
 
-    // const picturePath = `./src/assets/pictures/${slug}.png`;
-    // if (!(await exists(picturePath))) {
-    //   await download(picture, picturePath);
-    // }
+    const world = character.world;
+    const year = character.time;
+    const wikiUrl = character.wiki.url;
 
-    const newCharacter = {
-      name,
-      wiki: {
-        picture,
-        url: wikiUrl,
-      },
-      time,
+    const slug = _.camelCase(`${lastName}-${firstName}-${world}-${year}`);
+    const pictureUrl = character.wiki.picture;
+    const picturePath = `assets/pictures/${slug}.png`;
+    await download(pictureUrl, picturePath);
+
+    newData.push({
+      firstName,
+      lastName,
       world,
-      slug,
-    };
-
-    newData.push(newCharacter);
+      year,
+      wikiUrl,
+      picture: picturePath,
+    });
   });
+  console.info(newData);
 
-  await writeJson(newData, './src/_data/characters.json');
+  // await writeJson(newData, './src/_data/characters.json');
 })();
