bdb64537f8001c315c0158c1841fcf9ffa4acf59
Pixelastic <tim@pixelastic.com>
2020-08-08 00:10:05 +0200

    feat(list): Add complete list

--- run.js
+++ run.js
@@ -4,35 +4,30 @@ const pMap = require('golgoth/lib/pMap');
 // const exists = require('firost/lib/exists');
 const download = require('firost/lib/download');
 // const run = require('firost/lib/run');
-const writeJson = require('firost/lib/writeJson');
+// const writeJson = require('firost/lib/writeJson');
 const readJson = require('firost/lib/readJson');
 const _ = require('golgoth/lib/lodash');
 
 (async () => {
-  const characters = await readJson('./src/_data/characters.json');
-  const newData = [];
-  await pMap(characters, async (character) => {
-    const name = character.name;
-    const [firstName, lastName] = _.words(name);
+  const old = await readJson('./lib/data.json');
+  await pMap(
+    old,
+    async (character) => {
+      const name = character.name;
+      const [firstName, lastName] = _.words(name);
 
-    const world = character.world;
-    const year = character.time;
-    const wikiUrl = character.wiki.url;
+      const world = character.world;
+      const year = character.time;
 
-    const slug = _.camelCase(`${lastName}-${firstName}-${world}-${year}`);
-    const pictureUrl = character.wiki.picture;
-    const picturePath = `assets/pictures/${slug}.png`;
-    // await download(pictureUrl, `src/${picturePath}`);
-
-    newData.push({
-      firstName,
-      lastName,
-      world,
-      year,
-      wikiUrl,
-      picture: picturePath,
-    });
-  });
-
-  await writeJson(newData, './src/_data/characters.json');
+      const slug = _.camelCase(`${lastName}-${firstName}-${world}-${year}`);
+      const pictureUrl = character.wiki.picture.replace(
+        'scale-to-width-down/350',
+        ''
+      );
+      const picturePath = `assets/pictures/${slug}.png`;
+      console.info(name);
+      await download(pictureUrl, `src/${picturePath}`);
+    },
+    { concurrency: 1 }
+  );
 })();
--- src/_data/list.js
+++ src/_data/list.js
@@ -1,19 +1,10 @@
-const characters = require('../../lib/data.json');
+const characters = require('./characters.json');
 const _ = require('golgoth/lib/lodash');
 
 module.exports = async () => {
   const value = _.chain(characters)
-    .map((character) => {
-      const [firstName, lastName] = _.words(character.name);
-      return {
-        ...character,
-        firstName,
-        lastName,
-      };
-    })
     .sortBy(['lastName', 'firstName'])
     .groupBy('world')
     .value();
-  console.info(value);
   return value;
 };
