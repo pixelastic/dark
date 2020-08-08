2d185a9fcbd205d89308eb7a1a6d5dca778dc058
Pixelastic <tim@pixelastic.com>
2020-08-08 15:27:01 +0200

    wip: disable lazyload in cards

--- run.js
+++ run.js
@@ -1,27 +1,35 @@
 const mql = require('@microlink/mql');
-const read = require('firost/lib/read');
 const download = require('firost/lib/download');
 const exists = require('firost/lib/exists');
 const pMap = require('golgoth/lib/pMap');
+const characters = require('./src/_data/characters.json');
+const _ = require('golgoth/lib/lodash');
 
 (async () => {
-  const ids = (await read('./tmp/ids.txt')).split('\n');
   await pMap(
-    [ids[11]],
-    async (id) => {
-      console.info(id);
-      const { data } = await mql('https://projects.pixelastic.com/dark/', {
-        screenshot: true,
-        prerender: true,
-        force: true,
-        waitUntil: 'networkidle0',
-        element: `#${id}`,
-      });
-      const url = data.screenshot.url;
-      console.info(data.screenshot);
-      const card = `./cards/${id}.png`;
-      if (!(await exists(card))) {
-        await download(url, card);
+    [characters[6]],
+    async (character) => {
+      const id = _.camelCase(
+        `${character.year}-${character.lastName}-${character.firstName}-${character.id}`
+      );
+      const cardUrl = `https://projects.pixelastic.com/dark/cards/${id}/index.html`;
+      console.info(cardUrl);
+      try {
+        const { data } = await mql(cardUrl, {
+          screenshot: true,
+          force: true,
+          waitUntil: 'networkidle1',
+          element: '.__card',
+        });
+        console.info(data.screenshot);
+        const url = data.screenshot.url;
+        const card = `./cards/${id}.png`;
+        if (!(await exists(card))) {
+          await download(url, card);
+        }
+      } catch (err) {
+        console.info('Microlin error');
+        console.info(err);
       }
     },
     { concurrency: 2 }
